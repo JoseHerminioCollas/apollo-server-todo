d81382a662fd7adbd9199e1d108c135fd17fc68f
Jose Collas <joseherminiocollas@goatstonc.com>
2019-07-10 17:46:45 -0700

    factor out function

--- src/resolver/doer.js
+++ src/resolver/doer.js
@@ -5,24 +5,22 @@ function Doer(name, todos = [], todoIds = []) {
 }
 const doer = (model) => {
   const doers = []
-
   doers.push({ name: 'you' , todos: [model.todos[0]]}, { name: 'me', todos: [] })
-
-  function getOwnTodos(doerId) {
-    return model.todos.filter(e => 1 === e.id)
+  function getTodo(todoId) {
+    return  model.todos.filter(td => todoId === td.id)
   }
+  
   return {
     get: () => doers,
     add(name, todoIds) {
       // get the todos from the model and populate
       const todosArg = []
       for (let i = 0; i < todoIds.length; i++) {
-        const a = model.todos.filter(td => todoIds[i] === td.id)
-        todosArg.push(...a)
+        todosArg.push(...getTodo(todoIds[i]))
       }
-      const d = new Doer(name, todosArg, todoIds)
-      doers.push(d)
-      return d
+      const doer = new Doer(name, todosArg, todoIds)
+      doers.push(doer)
+      return doer
     },
   }
 }
